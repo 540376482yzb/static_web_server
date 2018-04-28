7778b79db588102416d75b54fd700b10bd4613a3
Zhou <540376482yzb@gmail.com>
2018-04-27 18:18:38 -0700

    serving files

---
+++ server.js
@@ -0,0 +1,18 @@
+const static = require('node-static')
+
+const file = new static.Server('./public')
+
+require('http')
+  .createServer(function(request, response) {
+    request
+      .addListener('end', function() {
+        //
+        // Serve files!
+        //
+        file.serve(request, response)
+      })
+      .resume()
+  })
+  .listen(8080, () => {
+    console.log('server started')
+  })
